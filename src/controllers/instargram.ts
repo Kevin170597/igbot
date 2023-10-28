7999dc75e9d1ab8ed3dded03554de4702b7fd4fc
Kevin Mendoza <kevin@xymilar.com>
2023-10-28 13:18:57 -0300

    ready-to-publis

--- src/controllers/instargram.ts
+++ src/controllers/instargram.ts
@@ -8,9 +8,10 @@ export const postAlbum = async (req: Request, res: Response) => {
         const hour = req.query.hour as string
         const day = moment().format("DD/MM/YYYY")
 
-        //const { urls, caption } = await getAlbumService(username, day, hour)
-        //const publish = await postAlbumService(username, urls, caption)
-        res.send({ day, username, hour })
+        const { urls, caption } = await getAlbumService(username, day, hour)
+        if (!urls || !caption) throw new Error("Album not found!")
+        const publish = await postAlbumService(username, urls, caption)
+        res.send({ day, username, hour, publish })
     } catch (error) {
         res.send(error)
     }
@@ -18,11 +19,12 @@ export const postAlbum = async (req: Request, res: Response) => {
 
 export const postPhoto = async (req: Request, res: Response) => {
     try {
-        const username = req.query.username as string
-        const day = req.query.day as string
+        const username = req.params.username as string
         const hour = req.query.hour as string
+        const day = moment().format("DD/MM/YYYY")
 
         const { url, caption } = await getPhotoService(username, day, hour)
+        if (!url || !caption) throw new Error("Photo not found!")
         const publish = await postPhotoService(username, url, caption)
         res.send({ url, caption, publish })
     } catch (error) {
--- src/routes/instagram.ts
+++ src/routes/instagram.ts
@@ -4,6 +4,6 @@ import { postAlbum, postPhoto } from "../controllers"
 const router = Router()
 
 router.get("/album/:username", postAlbum)
-router.get("/photo", postPhoto)
+router.get("/photo/:username", postPhoto)
 
 export { router }
